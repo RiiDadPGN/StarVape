e607f9bfa311421b49c5677ab457d6c6c9967468
RiiDadPGN <kardessblox@gmail.com>
2025-02-28 09:37:37 +0300

    Released Star Vape Beta

--- games/79695841807485.lua
+++ games/79695841807485.lua
@@ -51,7 +51,7 @@ run(function()
 
 		for name in map do
 			if not scripts[name] then
-				vape:CreateNotification('Vape', 'Unable to find script: '..name, 10, 'alert')
+				vape:CreateNotification('Star Vape', 'Unable to find script!: '..name, 10, 'alert')
 				return false
 			end
 		end
