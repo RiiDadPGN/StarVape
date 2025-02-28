7d6b1359d29b77bc7eef95084bc9f9ec9680e552
RiiDadPGN <kardessblox@gmail.com>
2025-02-28 09:41:39 +0300

    Released Star Vape Beta

--- games/8542275097.lua
+++ games/8542275097.lua
@@ -2,7 +2,7 @@ local vape = shared.vape
 local loadstring = function(...)
 	local res, err = loadstring(...)
 	if err and vape then 
-		vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert') 
+		vape:CreateNotification('Star Vape', 'Failed to load : '..err, 30, 'alert') 
 	end
 	return res
 end
@@ -15,13 +15,13 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function() 
-			return game:HttpGet('https://raw.githubusercontent.com/QP-Offcial/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true) 
+			return game:HttpGet('https://raw.githubusercontent.com/RiiDadPGN/StarVape/'..readfile('starvape/profiles/commit.txt')..'/'..select(1, path:gsub('starvape/', '')), true) 
 		end)
 		if not suc or res == '404: Not Found' then 
 			error(res) 
 		end
 		if path:find('.lua') then 
-			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res 
+			res = '--VAPE⭐ | Remove this file to make it persist after an update\n'..res 
 		end
 		writefile(path, res)
 	end
@@ -29,15 +29,15 @@ local function downloadFile(path, func)
 end
 
 vape.Place = 8768229691
-if isfile('newvape/games/'..vape.Place..'.lua') then
-	loadstring(readfile('newvape/games/'..vape.Place..'.lua'), 'skywars')()
+if isfile('starvape/games/'..vape.Place..'.lua') then
+	loadstring(readfile('starvape/games/'..vape.Place..'.lua'), 'skywars')()
 else
 	if not shared.VapeDeveloper then
 		local suc, res = pcall(function() 
-			return game:HttpGet('https://raw.githubusercontent.com/QP-Offcial/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/games/'..vape.Place..'.lua', true) 
+			return game:HttpGet('https://raw.githubusercontent.com/RiiDadPGN/StarVape/'..readfile('starvape/profiles/commit.txt')..'/games/'..vape.Place..'.lua', true) 
 		end)
 		if suc and res ~= '404: Not Found' then
-			loadstring(downloadFile('newvape/games/'..vape.Place..'.lua'), 'skywars')()
+			loadstring(downloadFile('starvape/games/'..vape.Place..'.lua'), 'skywars')()
 		end
 	end
 end
